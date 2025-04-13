e8d1f95a1d931652a7b52a55561698271c9cd26e
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-04-13 09:36:56 +0700

    chore: add type for icon

---
+++ src/constant/iconMap.ts
@@ -0,0 +1,14 @@
+import DailyDuaIcon from '@/assets/icon/daily-dua-icon.png';
+import DhikrAfterSalahIcon from '@/assets/icon/dhikr-after-sala.png';
+import EveningDhikrIcon from '@/assets/icon/evening-dhikr.png';
+import MorningDhikrIcon from '@/assets/icon/morning-dhikr.png';
+
+const IconMap: Record<string, string> = {
+  'daily-dua' : DailyDuaIcon,
+  'dhikr-after-salah': DhikrAfterSalahIcon,
+  'evening-dhikr': EveningDhikrIcon,
+  'morning-dhikr': MorningDhikrIcon
+
+}
+
+export default IconMap;
