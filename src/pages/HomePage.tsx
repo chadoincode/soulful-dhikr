17da05beb224b05f5c5cfa3f7fe4ee9282f2397f
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-04-13 19:11:50 +0700

    feat(homepage): add link to route page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Title from "../components/Title";
+import HomePageContainer from "../components/HomePage/HomePageContainer";
+
+const HomePage = () => {
+  return(
+    <>
+      <Title />
+      <HomePageContainer/>
+    </>
+  )
+}
+
+export default HomePage
