747159321c2c8146ed88ef13d910d228f04bf945
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-04-16 10:24:44 +0700

    feat(dua): add detail dua card

---
+++ src/components/DetailDua/DetailDuaContainer.tsx
@@ -0,0 +1,7 @@
+// import DetailDuaCard from "./DetailDuaCard";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router";
+
+// const  DetailDuaContainer = () => {
+//   const {duaId}
+// }
