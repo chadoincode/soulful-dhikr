ee87cb9c1a84734053a5516324ffb0bca0c97813
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-04-24 20:05:18 +0700

    style: create button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,8 @@ type ButtonProps = {
 
 const Button = ({text}: ButtonProps) => {
   return(
-    <div className="bg-secondary">
-      <p className="text-background">{text}</p>
+    <div className="bg-secondary w-[120px] md:w-[200px] rounded-lg p-3 cursor-pointer">
+      <p className="text-background text-lg font-semibold">{text}</p>
     </div>
   )
 }
--- src/pages/DetailDuaPage.tsx
+++ src/pages/DetailDuaPage.tsx
@@ -1,9 +1,14 @@
 import DetailDuaContainer from "../components/DetailDua/DetailDuaContainer";
+import Button from "../components/Button";
 
 const DetailDuaPage = () => {
   return(
     <div className="flex flex-col max-w-[330px] md:max-w-[600px]">
       <DetailDuaContainer />
+      <div className="mt-5 flex flex-row justify-between">
+        <Button text="Previous" />
+        <Button text="Next" />
+      </div>
     </div>
   )
 }
