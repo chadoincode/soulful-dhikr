4f4bda1c8e3ee90a71bac75459a6f6907207de50
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-04-29 14:23:41 +0700

    refactor: delete unuse code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,5 @@
 import Title from "../components/Title";
 import HomePageContainer from "../components/HomePage/HomePageContainer";
-import DhikrContainer from "../components/Dhikr/DhikrContainer";
-import DuaTitleContainer from "../components/Dua/DuaTitleContainer";
-import DuaPage from "./SelectedDuaPage";
 
 const HomePage = () => {
   return(
