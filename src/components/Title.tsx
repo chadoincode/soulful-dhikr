2f48f1ba165b7c98560d83eb7b0a410bbe4c9da8
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-04-30 13:53:30 +0700

    style: resize title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 const Title = () => {
   return(
-    <div className="flex justify-center items-center m-5 ">
-      <h1 className=" text-secondary text-2xl md:text-4xl text-center font-semibold">Soulful Dhikr</h1>
+    <div className="flex justify-center items-center m-5">
+      <h1 className=" text-secondary text-3xl md:text-4xl text-center font-semibold">Soulful Dhikr</h1>
     </div>
   )
 }
