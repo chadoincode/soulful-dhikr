1c2561e7ea753ca9b70ecea2ecec40f292cbf7e5
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-04-30 13:51:58 +0700

    style: change border radius card

--- src/components/Dua/DuaTitleCard.tsx
+++ src/components/Dua/DuaTitleCard.tsx
@@ -9,7 +9,7 @@ type DuaTitleCardProps = {
 const DuaTitleCard = ({title}: DuaTitleCardProps) => {
   return(
     <Link to={`/${title.category}/${title.id}`}>
-      <div className="flex justify-between items-center cursor-pointer bg-secondary w-[330px] h-[60px] py-2.5 px-5 rounded-3xl md:w-[600px]">
+      <div className="flex justify-between items-center cursor-pointer bg-secondary w-[330px] h-[60px] py-2.5 px-5 rounded-2xl md:w-[600px]">
         <div>
           <p className="text-sm font-bold text-background text-left line-clamp-2">{title.title}</p>
         </div>
