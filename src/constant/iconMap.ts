a1a5545d0512c306f6feb6ba86d5365afcc75c40
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-16 12:39:23 +0700

    refactor: change iconmap code, add alt for each icon

--- src/constant/iconMap.ts
+++ src/constant/iconMap.ts
@@ -4,12 +4,27 @@ import EveningDhikrIcon from '../assets/evening-dhikr.png';
 import MorningDhikrIcon from '../assets/morning-dhikr.png';
 import SelectedDuaIcon from '../assets/selected-dua-icon.png';
 
-const IconMap: Record<string, string> = {
-  'daily-dua' : DailyDuaIcon,
-  'dhikr-after-salah': DhikrAfterSalahIcon,
-  'evening-dhikr': EveningDhikrIcon,
-  'morning-dhikr': MorningDhikrIcon,
-  'selected-dua': SelectedDuaIcon,
+const IconMap: Record<string, {icon: string; alt:string}> = {
+  'daily-dua' : {
+    icon: DailyDuaIcon,
+    alt: 'Daily Dua Icon'
+  },
+  'dhikr-after-salah': {
+    icon: DhikrAfterSalahIcon,
+    alt: 'Dhikr After Salah Icon'
+  },
+  'evening-dhikr': {
+    icon: EveningDhikrIcon,
+    alt: 'Evening Dhikr Icon'
+  },
+  'morning-dhikr': {
+    icon: MorningDhikrIcon,
+    alt: 'Morning Dhikr Icon'
+  },
+  'selected-dua': {
+    icon: SelectedDuaIcon,
+    alt: 'Selected Dua Icon'
+  },
 }
 
 export default IconMap;
