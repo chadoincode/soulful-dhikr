0c598a6900967b7913273ef95f83abb7fd91b7b9
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-16 11:34:54 +0700

    chore: add seo on page in html tag

--- src/components/DetailDua/DetailDua.tsx
+++ src/components/DetailDua/DetailDua.tsx
@@ -3,6 +3,7 @@ import { Dua } from "../../types/dua";
 import Header from "../Header";
 import { Link } from "react-router";
 import { useParams } from "react-router";
+import { Helmet } from "react-helmet";
 
 type DetailDuaProps = {
   dua: Dua
@@ -12,6 +13,9 @@ const DetailDua = ({dua}: DetailDuaProps) => {
   const { slug } = useParams<{slug: string}>()
   return(
     <div>
+      <Helmet>
+        <meta name="description" content={`Content from ${dua.title}`} />
+      </Helmet>
       <Link to={`/${slug}`}>
         <Header title={dua.title}/>
       </Link>
